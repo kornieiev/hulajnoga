5192293364e262f62baa781e9ffebefe16968b17
Dima Kornieiev <108156304+kornieiev@users.noreply.github.com>
2023-12-14 10:02:25 +0100

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
+import { Header, Header2, Image } from './App.styled';
+
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
+        width: '500px',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
+        flexDirection: 'column',
+        alignItems: 'normal',
+        marginLeft: 'auto',
+        marginRight: 'auto',
       }}
     >
-      React homework template
+      <Header>
+        Pobliżu szkoły 340
+        <br />
+        zaginęła hulajnoga
+      </Header>
+      <Header2>Proszę zwrócić go właścicielowi</Header2>
+      <Header2>723-377-289</Header2>
+
+      <Image src={require('../img/pic1.jpg')} alt="pic1" />
+      <Image src={require('../img/pic2.jpg')} alt="pic2" />
+      <Image src={require('../img/pic3.jpg')} alt="pic3" />
+      <Image src={require('../img/sz340-1-min.jpg')} alt="pic4" />
+      <Image src={require('../img/sz340-2-min.jpg')} alt="pic5" />
     </div>
   );
 };
---
+++ src/components/App.styled.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const Image = styled.img`
+  width: 450px;
+  border: 1px solid grey;
+  margin-bottom: 5px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+export const Header = styled.h1`
+  width: 450px;
+  font-size: 50px;
+  color: red;
+  /* border: 2px solid blue; */
+  margin-bottom: 3px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+export const Header2 = styled.h2`
+  justify-content: center;
+  width: 450px;
+  font-size: 30px;
+  color: brown;
+  margin-bottom: 2px;
+  margin-top: 2px;
+  margin-left: auto;
+  margin-right: auto;
+  display: flex;
+`;
