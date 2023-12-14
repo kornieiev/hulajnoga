14a30bbcf8a403ede142b1053db72628bf42ccb0
Dima Kornieiev <108156304+kornieiev@users.noreply.github.com>
2023-12-14 10:15:52 +0100

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,19 +16,22 @@ export const App = () => {
         marginRight: 'auto',
       }}
     >
-      <Header>
-        Pobliżu szkoły 340
-        <br />
-        zaginęła hulajnoga
-      </Header>
+      <Header>Na terenie szkoły 340</Header>
+      <Header>zaginęła hulajnoga</Header>
       <Header2>Proszę zwrócić go właścicielowi</Header2>
-      <Header2>723-377-289</Header2>
+      <Header2>
+        <b>723-377-289</b>
+      </Header2>
 
+      <Image src={require('../img/1.jpg')} alt="1" />
       <Image src={require('../img/pic1.jpg')} alt="pic1" />
       <Image src={require('../img/pic2.jpg')} alt="pic2" />
       <Image src={require('../img/pic3.jpg')} alt="pic3" />
       <Image src={require('../img/sz340-1-min.jpg')} alt="pic4" />
       <Image src={require('../img/sz340-2-min.jpg')} alt="pic5" />
+      <Header2>
+        <b>Bardzo dziękuję za pomoc!</b>
+      </Header2>
     </div>
   );
 };
--- src/components/App.styled.js
+++ src/components/App.styled.js
@@ -1,31 +1,34 @@
 import styled from 'styled-components';
 
 export const Image = styled.img`
-  width: 450px;
-  border: 1px solid grey;
+  width: 320px;
+  border: 0.5px solid grey;
   margin-bottom: 5px;
   margin-left: auto;
   margin-right: auto;
 `;
 
 export const Header = styled.h1`
+  display: flex;
+  justify-content: center;
   width: 450px;
-  font-size: 50px;
+  font-size: 36px;
   color: red;
   /* border: 2px solid blue; */
-  margin-bottom: 3px;
+  margin-top: 5px;
+  margin-bottom: 0px;
   margin-left: auto;
   margin-right: auto;
 `;
 
 export const Header2 = styled.h2`
+  display: flex;
   justify-content: center;
   width: 450px;
-  font-size: 30px;
+  font-size: 24px;
   color: brown;
-  margin-bottom: 2px;
-  margin-top: 2px;
+  margin-bottom: 5px;
+  margin-top: 3px;
   margin-left: auto;
   margin-right: auto;
-  display: flex;
 `;
